66fd29476dfc76f768cd495971a732cf86ab599b
MattixNow <matteo.gauthier@gmail.com>
2018-04-12 20:18:38 +0200

    add php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+</head>
+
+<body>
+<?php $Parsedown = new Parsedown();
+echo $Parsedown->text('Hello _Parsedown_!'); ?>
+</body>
+
+</html>
