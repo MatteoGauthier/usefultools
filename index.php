1de316903636a855c0df53def93fafb264341555
MattixNow <matteo.gauthier@gmail.com>
2018-04-23 19:30:47 +0200

    minor

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Outils Utiles | UsefulTools</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 <link rel="stylesheet" href="css/master.css">
+<meta http-equiv="refresh" content="1; URL=https://www.fogview.web-edu.fr/usefultools/home.php">
 </head>
 
 <body>
