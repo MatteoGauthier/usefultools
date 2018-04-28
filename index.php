b66752f6dabcbc533d32bb932e0259927bc2c3d7
MattixNow <matteo.gauthier@gmail.com>
2018-04-28 16:20:00 +0200

    add

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="google-site-verification" content="lGoxoFkE_FwhlmtXnaTMbLsnUAeEUqwTgMOT3mwblM8" />
+  
   <title>Outils Utiles | UsefulTools</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 <link rel="stylesheet" href="css/master.css">
