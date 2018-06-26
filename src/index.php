b5960d6162c905baae428ab768fbfdd5b2884a9a
Mattèo Gauthier <matteo.gauthier@gmail.com>
2018-06-26 12:27:52 +0200

    commit for organize projet + X push + test gitignore + later better org

--- src/index.php
+++ src/index.php
@@ -1,50 +1,18 @@
 <html>
-
-<head>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="ressources_front/style/github-markdown.css">
-    <style>
-        .markdown-body {
-            box-sizing: border-box;
-            min-width: 200px;
-            max-width: 980px;
-            margin: 0 auto;
-            padding: 45px;
-        }
-        
-        @media (max-width: 767px) {
-            .markdown-body {
-                padding: 15px;
-            }
-        }
-        </style>
-        <link rel="stylesheet" href="ressources_front/style/master.css">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118325156-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-118325156-1');
-</script>
-
-</head>
+    <?php include('ressources_back\include_head.php') ?>
 
 <body>
-
-
     <?php 
 require "vendor/autoload.php";
-$text = file_get_contents('ressources_back/markdown/home.md');  
+$UFT_home = file_get_contents('ressources_back/markdown/UFT_home.md');  
 
 $Extra = new ParsedownExtra();
-echo('<article class="markdown-body"><div class="content">');
-echo $Extra->text($text);
+echo('<article class="markdown-body"><div id="content">');
+echo $Extra->text($UFT_home);
 echo('</div></article>')
 ?>
-
+<?php include('ressources_back\include_js.php') ?>
+<script src="https://unpkg.com/embed-js@5.0.3/umd/embed.js"></script>
 </body>
 
 </html>
